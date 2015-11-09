730307ecbcd10c4c70215851d4f7558961fa818e
条件状语从句 <lixzap@gmail.com>
2015-11-09 12:20:04 +0800

    info-base界面

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
-var cityname = $.cookie('CITY_NAME');
+// var cityname = $.cookie('CITY_NAME');
+var cityname = "hangzhou"
+
 
 $(document).ready(function() {
 	// checkCookie();
+	$('.info-base').css('height',$(window).height()-65);
 });
 
 
@@ -35,8 +38,8 @@ avalon.ready(function() {
 
 function getInfo(city) {
 	$.ajax({
-		url : 'http://apis.baidu.com/heweather/weather/free?city='+city,
-		// url : 'testData.json',
+		// url : 'http://apis.baidu.com/heweather/weather/free?city='+city,
+		url : 'testData.json',
 		dataType : 'json',
 		type : "get",
 		async : false,
@@ -45,6 +48,7 @@ function getInfo(city) {
 		},
 		success : function(data) {
 			if(data['HeWeather data service 3.0'][0].status=='ok'){
+				$('.form-inoput-city').fadeOut(300);
 				vmodel.result = data['HeWeather data service 3.0'][0];
 			}
 			else if(data['HeWeather data service 3.0'][0].status=='unknown city'){
@@ -55,57 +59,3 @@ function getInfo(city) {
 	});
 }
 
-
-
-// function getCookie(c_city)
-// {
-// 	if (document.cookie.length>0)
-// 	{
-// 		c_start=document.cookie.indexOf(c_city + "=")
-// 		if (c_start!=-1)
-// 		{
-// 			c_start=c_start + c_city.length+1
-// 			c_end=document.cookie.indexOf(";",c_start)
-// 			if (c_end==-1) c_end=document.cookie.length
-// 				return unescape(document.cookie.substring(c_start,c_end))
-// 		}
-// 	}
-// 	return ""
-// }
-
-// function setCookie(c_city,value,expiredays)
-// {
-// 	var exdate=new Date()
-// 	exdate.setDate(exdate.getDate()+expiredays)
-// 	document.cookie=c_city+ "=" +escape(value)+
-// 	((expiredays==null) ? "" : "; expires="+exdate.toGMTString())
-// }
-
-
-// function checkCookie() {
-// 	cityName=getCookie('cityName')
-// 	if (cityName!=null && cityName!="")
-// 	{
-// 			// cookie中已经存在城市名
-// 			alert(cityname);
-// 		}
-// 		else
-// 		{
-// 			$('.form-inoput-city').show();
-
-// 			$('#btn-ok').click(function(){
-// 				var inputCity = $('[name=input-city-name]').val();
-// 				if(inputCity==""){
-// 					alert('请输入城市名!');
-// 					return;
-// 				}
-// 				cityName = inputCity;
-// 				if (cityName!=null && cityName!=""){
-// 					getInfo(cityname);
-// 					setCookie('cityName',cityName,365);
-// 				}
-// 			});
-
-
-// 		}
-// 	}
